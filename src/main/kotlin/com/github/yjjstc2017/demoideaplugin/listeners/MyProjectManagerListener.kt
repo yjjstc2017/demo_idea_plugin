fc8e407d005912fb668049e7b169e63e4c962b7b
GitHub Action <action@github.com>
2022-08-14 07:58:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/yjjstc2017/demoideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yjjstc2017/demoideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yjjstc2017.demoideaplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yjjstc2017.demoideaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/yjjstc2017/demoideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yjjstc2017/demoideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yjjstc2017.demoideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yjjstc2017.demoideaplugin.MyBundle
 
 class MyApplicationService {
 
